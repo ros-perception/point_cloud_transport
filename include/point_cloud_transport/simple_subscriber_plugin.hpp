6d57fb268535cfe8ab9c3882cec55917b9fdcf00
john-maidbot <john.dangelo@tailos.com>
2023-08-05 18:36:11 -0500

    Fudge

--- include/point_cloud_transport/simple_subscriber_plugin.hpp
+++ include/point_cloud_transport/simple_subscriber_plugin.hpp
@@ -102,7 +102,7 @@ public:
   }
 
   void setParamCallback(
-    rclcpp::node_interfaces::NodeParametersInterface::OnParametersSetCallbackType
+    rclcpp::node_interfaces::NodeParametersInterface::OnSetParametersCallbackType
     param_change_callback)
   {
     if (impl_) {
