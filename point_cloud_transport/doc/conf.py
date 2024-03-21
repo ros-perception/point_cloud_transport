41e6749a02813aef05be3859db7ce65f34f78282
Alejandro Hernández Cordero <ahcorde@gmail.com>
2024-03-21 11:13:02 +0100

    use README.md
    Signed-off-by: Alejandro Hernández Cordero <ahcorde@gmail.com>

--- point_cloud_transport/doc/conf.py
+++ point_cloud_transport/doc/conf.py
@@ -57,6 +57,7 @@ extensions = [
     'sphinx.ext.doctest',
     'sphinx.ext.coverage',
     'sphinx_rtd_theme',
+    "myst_parser",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
