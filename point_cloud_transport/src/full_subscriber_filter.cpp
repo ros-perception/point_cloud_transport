b77e5e1038873d57a53d1501b4b26f04cc654d6e
elsayedelsheikh <elsayed.elsheikh97@gmail.com>
2025-06-26 19:56:29 +0000

    make copyright test happy
    Signed-off-by: elsayedelsheikh <elsayed.elsheikh97@gmail.com>

--- point_cloud_transport/src/full_subscriber_filter.cpp
+++ point_cloud_transport/src/full_subscriber_filter.cpp
@@ -1,3 +1,4 @@
+// Copyright (c) 2025, Nobody?
 //
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions are met:
