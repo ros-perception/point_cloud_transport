613d980494cc612347cf2ec7e110095fb8631e17
elsayedelsheikh <elsayed.elsheikh97@gmail.com>
2025-06-26 19:28:33 +0000

    New subscriberfilter class
    Signed-off-by: elsayedelsheikh <elsayed.elsheikh97@gmail.com>

--- point_cloud_transport/include/point_cloud_transport/full_subscriber_filter.hpp
+++ point_cloud_transport/include/point_cloud_transport/full_subscriber_filter.hpp
@@ -1,3 +1,31 @@
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+//    * Redistributions of source code must retain the above copyright
+//      notice, this list of conditions and the following disclaimer.
+//
+//    * Redistributions in binary form must reproduce the above copyright
+//      notice, this list of conditions and the following disclaimer in the
+//      documentation and/or other materials provided with the distribution.
+//
+//    * Neither the name of the copyright holder nor the names of its
+//      contributors may be used to endorse or promote products derived from
+//      this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+//
+
 #ifndef POINT_CLOUD_TRANSPORT__FULL_SUBSCRIBER_FILTER_HPP_
 #define POINT_CLOUD_TRANSPORT__FULL_SUBSCRIBER_FILTER_HPP_
 
@@ -9,6 +37,7 @@
 #include <sensor_msgs/msg/point_cloud2.hpp>
 
 #include <point_cloud_transport/point_cloud_transport.hpp>
+#include <point_cloud_transport/subscriber_filter.hpp>
 #include <point_cloud_transport/transport_hints.hpp>
 #include "point_cloud_transport/visibility_control.hpp"
 
@@ -31,7 +60,7 @@ namespace point_cloud_transport
 ///
 class FullSubscriberFilter
   : public message_filters::SubscriberBase,
-  public message_filters::SimpleFilter<sensor_msgs::msg::PointCloud2>
+  public SubscriberFilter
 {
 public:
   POINT_CLOUD_TRANSPORT_PUBLIC
@@ -43,7 +72,7 @@ public:
       rclcpp::node_interfaces::NodeLoggingInterface>> node_interfaces,
     const std::string & base_topic,
     const std::string & transport,
-    const rclcpp::QoS & custom_qos,
+    rclcpp::QoS custom_qos,
     rclcpp::SubscriptionOptions options = rclcpp::SubscriptionOptions());
 
   template<typename NodeT = rclcpp::Node::SharedPtr>
@@ -52,7 +81,7 @@ public:
     NodeT node,
     const std::string & base_topic,
     const std::string & transport,
-    const rclcpp::QoS & custom_qos,
+    rclcpp::QoS custom_qos,
     rclcpp::SubscriptionOptions options = rclcpp::SubscriptionOptions())
   {
     auto node_interfaces = std::make_shared<rclcpp::node_interfaces::NodeInterfaces<
@@ -65,15 +94,17 @@ public:
 
   //! Empty constructor, use subscribe() to subscribe to a topic
   POINT_CLOUD_TRANSPORT_PUBLIC
-  FullSubscriberFilter();
+  FullSubscriberFilter() {}
 
+  //! Empty constructor, parent class already calls unsubscribe()
   POINT_CLOUD_TRANSPORT_PUBLIC
-  ~FullSubscriberFilter();
+  ~FullSubscriberFilter() {}
 
   ///
   /// \brief Subscribe to a topic. If this Subscriber is already subscribed to a topic,
   /// this function will first unsubscribe.
-  /// \param node The rclcpp Node to use to subscribe.
+  /// \param node_interfaces the ROS node interfaces required for core node functionality, including
+  ///    NodeBaseInterface, NodeParametersInterface, NodeTopicsInterface, and NodeLoggingInterface.
   /// \param base_topic The topic to subscribe to.
   /// \param transport The transport hint to pass along
   /// \param custom_qos Custom quality of service
@@ -88,8 +119,8 @@ public:
       rclcpp::node_interfaces::NodeLoggingInterface>> node_interfaces,
     const std::string & base_topic,
     const std::string & transport,
-    const rclcpp::QoS & custom_qos,
-    rclcpp::SubscriptionOptions options = rclcpp::SubscriptionOptions());
+    rclcpp::QoS custom_qos,
+    rclcpp::SubscriptionOptions options = rclcpp::SubscriptionOptions()) override;
 
   template<typename NodeT = rclcpp::Node::SharedPtr>
   POINT_CLOUD_TRANSPORT_PUBLIC
@@ -97,7 +128,7 @@ public:
     NodeT node,
     const std::string & base_topic,
     const std::string & transport,
-    const rclcpp::QoS & custom_qos,
+    rclcpp::QoS custom_qos,
     rclcpp::SubscriptionOptions options = rclcpp::SubscriptionOptions())
   {
     auto node_interfaces = std::make_shared<rclcpp::node_interfaces::NodeInterfaces<
@@ -119,7 +150,8 @@ public:
     rclcpp::SubscriptionOptions /*options*/) override
   {}
 
-  //! Re-subscribe to a topic.  Only works if this subscriber has previously been subscribed to a topic.
+  //! Re-subscribe to a topic.
+  // Only works if this subscriber has previously been subscribed to a topic.
   POINT_CLOUD_TRANSPORT_PUBLIC
   void subscribe() override;
 
@@ -127,28 +159,7 @@ public:
   POINT_CLOUD_TRANSPORT_PUBLIC
   void unsubscribe() override;
 
-  POINT_CLOUD_TRANSPORT_PUBLIC
-  std::string getTopic() const;
-  
-  //! Returns the number of publishers this subscriber is connected to.
-  POINT_CLOUD_TRANSPORT_PUBLIC
-  uint32_t getNumPublishers() const;
-
-  //! Returns the name of the transport being used.
-  POINT_CLOUD_TRANSPORT_PUBLIC
-  std::string getTransport() const;
-
-  //! Returns the internal point_cloud_transport::Subscriber object.
-  POINT_CLOUD_TRANSPORT_PUBLIC
-  const Subscriber & getSubscriber() const;
-
 private:
-  void cb(const sensor_msgs::msg::PointCloud2::ConstSharedPtr & m)
-  {
-    this->signalMessage(m);
-  }
-
-  Subscriber sub_;
   std::shared_ptr<rclcpp::node_interfaces::NodeInterfaces<
       rclcpp::node_interfaces::NodeBaseInterface,
       rclcpp::node_interfaces::NodeParametersInterface,
--- point_cloud_transport/src/full_subscriber_filter.cpp
+++ point_cloud_transport/src/full_subscriber_filter.cpp
@@ -1,3 +1,31 @@
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+//    * Redistributions of source code must retain the above copyright
+//      notice, this list of conditions and the following disclaimer.
+//
+//    * Redistributions in binary form must reproduce the above copyright
+//      notice, this list of conditions and the following disclaimer in the
+//      documentation and/or other materials provided with the distribution.
+//
+//    * Neither the name of the copyright holder nor the names of its
+//      contributors may be used to endorse or promote products derived from
+//      this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+//
+
 #include "point_cloud_transport/full_subscriber_filter.hpp"
 
 #include <memory>
@@ -13,19 +41,10 @@ FullSubscriberFilter::FullSubscriberFilter(
     rclcpp::node_interfaces::NodeLoggingInterface>> node_interfaces,
   const std::string & base_topic,
   const std::string & transport,
-  const rclcpp::QoS & custom_qos,
+  rclcpp::QoS custom_qos,
   rclcpp::SubscriptionOptions options)
 {
   subscribe(node_interfaces, base_topic, transport, custom_qos, options);
-}
-
-FullSubscriberFilter::FullSubscriberFilter()
-{
-}
-
-FullSubscriberFilter::~FullSubscriberFilter()
-{
-  unsubscribe();
 }
 
 void FullSubscriberFilter::subscribe(
@@ -36,7 +55,7 @@ void FullSubscriberFilter::subscribe(
     rclcpp::node_interfaces::NodeLoggingInterface>> node_interfaces,
   const std::string & base_topic,
   const std::string & transport,
-  const rclcpp::QoS & custom_qos,
+  rclcpp::QoS custom_qos,
   rclcpp::SubscriptionOptions options)
 {
   unsubscribe();
@@ -44,7 +63,7 @@ void FullSubscriberFilter::subscribe(
     node_interfaces, base_topic,
     std::bind(&FullSubscriberFilter::cb, this, std::placeholders::_1),
     transport, custom_qos, options);
-  
+
   this->node_interfaces_ = node_interfaces;
   this->topic_ = base_topic;
   this->transport_ = transport;
@@ -61,26 +80,7 @@ void FullSubscriberFilter::subscribe()
 
 void FullSubscriberFilter::unsubscribe()
 {
-  sub_.shutdown();
+  SubscriberFilter::unsubscribe();
 }
 
-std::string FullSubscriberFilter::getTopic() const
-{
-  return this->topic_;
-}
-
-uint32_t FullSubscriberFilter::getNumPublishers() const
-{
-  return sub_.getNumPublishers();
-}
-
-std::string FullSubscriberFilter::getTransport() const
-{
-  return this->transport_;
-}
-
-const Subscriber & FullSubscriberFilter::getSubscriber() const
-{
-  return sub_;
-}
 }  // namespace point_cloud_transport
