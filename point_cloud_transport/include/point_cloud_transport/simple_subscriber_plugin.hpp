cc0ea09bdebe3b6e55327d3c5748f072175b15a6
Alejandro Hernandez Cordero <ahcorde@gmail.com>
2025-10-14 17:39:07 +0200

    Fixed merge
    Signed-off-by: Alejandro Hernandez Cordero <ahcorde@gmail.com>

--- point_cloud_transport/include/point_cloud_transport/simple_subscriber_plugin.hpp
+++ point_cloud_transport/include/point_cloud_transport/simple_subscriber_plugin.hpp
@@ -92,12 +92,7 @@ public:
   bool getParam(const std::string & parameter_name, T & value) const
   {
     if (impl_) {
-<<<<<<< HEAD
-      uint ns_len = impl_->node_->get_effective_namespace().length();
-=======
-      unsigned int ns_len =
-        strlen(impl_->node_interfaces_.get_node_base_interface()->get_namespace());
->>>>>>> c03fc3b (Use standard unsigned int in place of uint for Windows compatibility (#134))
+      unsigned int ns_len = impl_->node_->get_effective_namespace().length();
       std::string param_base_name = getTopic().substr(ns_len);
       std::replace(param_base_name.begin(), param_base_name.end(), '/', '.');
 
@@ -115,12 +110,7 @@ public:
     rcl_interfaces::msg::ParameterDescriptor())
   {
     if (impl_) {
-<<<<<<< HEAD
-      uint ns_len = impl_->node_->get_effective_namespace().length();
-=======
-      unsigned int ns_len =
-        strlen(impl_->node_interfaces_.get_node_base_interface()->get_namespace());
->>>>>>> c03fc3b (Use standard unsigned int in place of uint for Windows compatibility (#134))
+      unsigned int ns_len = impl_->node_->get_effective_namespace().length();
       std::string param_base_name = getTopic().substr(ns_len);
       std::replace(param_base_name.begin(), param_base_name.end(), '/', '.');
 
