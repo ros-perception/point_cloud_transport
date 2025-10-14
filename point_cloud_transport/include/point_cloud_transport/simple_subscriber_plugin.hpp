59a18cb2f0ff1dea44b5c4e5ede8195b17592efe
Silvio Traversaro <silvio@traversaro.it>
2025-10-14 14:57:48 +0200

    Use standard unsigned int in place of uint for Windows compatibility (#134)
    (cherry picked from commit c03fc3b46b12e6ff14c79001e359583dc1d5e3ec)
    
    # Conflicts:
    #	point_cloud_transport/include/point_cloud_transport/simple_publisher_plugin.hpp
    #	point_cloud_transport/include/point_cloud_transport/simple_subscriber_plugin.hpp

--- point_cloud_transport/include/point_cloud_transport/simple_subscriber_plugin.hpp
+++ point_cloud_transport/include/point_cloud_transport/simple_subscriber_plugin.hpp
@@ -92,7 +92,12 @@ public:
   bool getParam(const std::string & parameter_name, T & value) const
   {
     if (impl_) {
+<<<<<<< HEAD
       uint ns_len = impl_->node_->get_effective_namespace().length();
+=======
+      unsigned int ns_len =
+        strlen(impl_->node_interfaces_.get_node_base_interface()->get_namespace());
+>>>>>>> c03fc3b (Use standard unsigned int in place of uint for Windows compatibility (#134))
       std::string param_base_name = getTopic().substr(ns_len);
       std::replace(param_base_name.begin(), param_base_name.end(), '/', '.');
 
@@ -110,7 +115,12 @@ public:
     rcl_interfaces::msg::ParameterDescriptor())
   {
     if (impl_) {
+<<<<<<< HEAD
       uint ns_len = impl_->node_->get_effective_namespace().length();
+=======
+      unsigned int ns_len =
+        strlen(impl_->node_interfaces_.get_node_base_interface()->get_namespace());
+>>>>>>> c03fc3b (Use standard unsigned int in place of uint for Windows compatibility (#134))
       std::string param_base_name = getTopic().substr(ns_len);
       std::replace(param_base_name.begin(), param_base_name.end(), '/', '.');
 
