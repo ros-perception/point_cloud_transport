20acd8bc1d694af887254746eb5d6a172fe9f67d
Alejandro Hernández Cordero <ahcorde@gmail.com>
2026-09-01 14:21:35 +0200

    Removed manifest.cpp and moved to cpp file (#192)
    Signed-off-by: Alejandro Hernandez Cordero <ahcorde@gmail.com>

--- point_cloud_transport/src/raw_publisher.cpp
+++ point_cloud_transport/src/raw_publisher.cpp
@@ -31,10 +31,5 @@
 
 #include <point_cloud_transport/publisher_plugin.hpp>
 #include <point_cloud_transport/raw_publisher.hpp>
-#include <point_cloud_transport/raw_subscriber.hpp>
-#include <point_cloud_transport/subscriber_plugin.hpp>
 
 PLUGINLIB_EXPORT_CLASS(point_cloud_transport::RawPublisher, point_cloud_transport::PublisherPlugin)
-PLUGINLIB_EXPORT_CLASS(
-  point_cloud_transport::RawSubscriber,
-  point_cloud_transport::SubscriberPlugin)
